066d9982b4430d9f9d22512d38b4c4b7affcffb7
esoshyki <esoshyki@yandex.by>
2019-09-23 22:18:05 +0300

    solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function multiply(first, second) {
-  // your solution
-}
+    return first.split('').reduceRight((p, a, i) => 
+        second.split('').reduceRight((p, b, j) => {
+          const mul = (a - '0') * (b - '0');
+          const p1 = i + j;
+          const p2 = p1 + 1;
+          const sum = mul + zer(p[p2]);
+                  
+          p[p1] = zer(p[p1]) + Math.floor(sum / 10);
+          p[p2] = sum % 10;
+                  
+          return p;
+        }, p)
+      , []).join('').replace(/^0+(?=\d)/, '');
+    }
+  
+  function zer(v) {
+    return v || 0;
+  }
